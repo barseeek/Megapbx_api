8e7cc9ce325daff92e0e13dc88ddd6b89a464522
Nikita <barskii.no@outlook.com>
2023-11-02 16:36:38 +0300

    Added number availability check

--- main.py
+++ main.py
@@ -9,27 +9,37 @@ def main():
 }
     domain = "vats730382.megapbx.ru"    
     load_dotenv()
-    #print(get_users(header, domain))
-    tel_nums = get_telnums(header, domain)
-    print(tel_nums['info'])
+    current_number = input('Input ext number\n').strip()
+    while not(current_number.isdigit()):
+        current_number = input('Input ext number\n')
+    users = get_users(header, domain)
+    print(is_number_free(users,current_number))
 
 
 def get_users(header, domain):
-    response = requests.get(f"https://{domain}/crmapi/v1/users", headers=header)
+    params = {
+        'search': '',
+        'start' : 0,
+        'limit' : 200
+    }
+    response = requests.get(f"https://{domain}/crmapi/v1/users", headers=header, params=params)
     response.raise_for_status()
-    return response.json()
+    return response.json()['items']
 
+
+def is_number_free(employees, number):
+    for employee in employees:
+        if employee['ext'] == number:
+            return False
+    return True
 
 def get_telnums(header, domain):
     params = {
         'search': '',
-        'start' : 10,
-        'limit' : 100
+        'start' : 101,
+        'limit' : 99
     }
-    response = requests.get(f"https://{domain}/crmapi/v1/telnums", headers=header)
-    print(response.url)
-    response.raise_for_status()
-    print(response.content)
+    response = requests.get(f"https://{domain}/crmapi/v1/telnums", headers=header, params=params)
     return response.json()
 
 if __name__=='__main__':
