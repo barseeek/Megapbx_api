d8cd681c9bd86fcb7a238efe8c622e0f2d1c40a2
Nikita <barskii.no@outlook.com>
2023-11-13 11:53:58 +0300

    Fix to flake8

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ import requests
 import os
 from dotenv import load_dotenv
 
+
 def main():
-    token = os.environ["MEGAPBX_TOKEN"]
-    header = {
-        'X-API-KEY' : token
-}
-    domain = "vats730382.megapbx.ru"    
     load_dotenv()
-    '''
-    current_number = input('Input ext number\n').strip()
-    while not(current_number.isdigit()):
-        current_number = input('Input ext number\n')
-    '''
+    megapbx_token = os.environ["MEGAPBX_TOKEN"]
+    header = {
+        'X-API-KEY': megapbx_token
+    }
+    domain = "vats730382.megapbx.ru"
     users = get_users(header, domain)
     free_numbers = get_free_numbers(users)
     print(free_numbers)
@@ -22,10 +18,12 @@ def main():
 def get_users(header, domain):
     params = {
         'search': '',
-        'start' : 0,
-        'limit' : 200
+        'start': 0,
+        'limit': 200
     }
-    response = requests.get(f"https://{domain}/crmapi/v1/users", headers=header, params=params)
+    response = requests.get(f"https://{domain}/crmapi/v1/users",
+                            headers=header,
+                            params=params)
     response.raise_for_status()
     return response.json()['items']
 
@@ -39,8 +37,8 @@ def is_number_free(employees, number):
 
 def get_free_numbers(users):
     free_numbers = []
-    for number in range(105,500):
-        if is_number_free(users,number):
+    for number in range(105, 500):
+        if is_number_free(users, number):
             free_numbers.append(number)
     return free_numbers
 
@@ -48,11 +46,14 @@ def get_free_numbers(users):
 def get_telnums(header, domain):
     params = {
         'search': '',
-        'start' : 101,
-        'limit' : 99
+        'start': 101,
+        'limit': 99
     }
-    response = requests.get(f"https://{domain}/crmapi/v1/telnums", headers=header, params=params)
+    response = requests.get(f"https://{domain}/crmapi/v1/telnums",
+                            headers=header,
+                            params=params)
     return response.json()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
